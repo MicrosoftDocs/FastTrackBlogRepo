3c2e176427bb4d9c79bc236d0325f30ae5de6fb4
petkra-ms <137082101+petkra-ms@users.noreply.github.com>
2025-05-05 11:33:14 +0200

    initial version

---
+++ Fabric-less Export/BPSTest/BpsPathItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BPSTest
+{
+    public class BpsPathItem
+    {
+        public required string Name { get; set; }
+
+        public bool? IsDirectory { get; set; }
+
+        public long? ContentLength { get; set; }
+
+        public DateTime? LastModified { get; set; }
+    }
+}
---
+++ Fabric-less Export/BPSTest/EntityType.cs
@@ -0,0 +1,15 @@
+﻿namespace BPSTest
+{
+    public class EntityType
+    {
+        public EntityType(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+    }
+}
